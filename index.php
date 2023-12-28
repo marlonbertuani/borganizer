22564725b1a339aad2248ce46b97f09c92f53aa6
Marlon Bertuani <31507265+marlonbertuani@users.noreply.github.com>
2023-12-28 03:23:32 +0000

    Atualizando a home page.

--- index.php
+++ index.php
@@ -5,66 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, max-scale=1.0">
     <title>Super Arthur</title>
     <link rel="stylesheet" href="css/style.css">
-    <style>
-        .logoarthur{
-            display: block;
-            margin-top: 60px;
-            margin-left: auto;
-            margin-right: auto;
-            width: 70%;
-        }
-
-        .msghome{
-            display: none;
-            color: rgb(223, 18, 18);
-            text-align: center;
-            font-family: Comic Sans MS;
-            margin-top: 120px;
-        }
-
-        .botoes{
-            margin-top: 220px;
-            font-family: Comic Sans MS;
-            background-color: red;
-            border: none;
-            color: #fff;
-            padding: 18px 35px;
-            text-align: center;
-            text-decoration: none;
-            font-weight: bold;
-            display: inline-block;
-            font-size: 22px;
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 8px;
-            animation: pulse 2s infinite;
-        }
-        @keyframes pulse {
-            0% {
-                transform: scale(1);
-            }
-            50% {
-                transform: scale(1.05);
-            }
-            100% {
-                transform: scale(1);
-            }
-        }
-
-        .botoes:hover {
-            animation: none;
-        }
-
-    </style>
 </head>
-<body>    
-    <img class="logoarthur" src="superarthur.png" style="text-align: center;">
-    <h1 class="msghome">Estou feliz com sua visita!</h1>
-    <br>
-    <br>
+<body>
+    <div class="image-container">
+        <img class="logoarthur" src="superarthur.png" alt="Super Arthur Logo">
+    </div>    
+    <div class="content-container">
+        <h1 class="msghome">Aniversário do Arthur!</h1>
+        <h1 style="font-size: large; text-align: center;">Confirme sua presença e nos ajude a organizar este momento especial!</h1>
+        <p style="font-size: large; text-align: center;">Prazo para confirmações até 17/01/2024 - 23:55</p>
+        
+        <div class="container">
+            <img class="calendar-img" src="img/calendar.png" alt="calendar logo">
+            <p class="text-next-to-image">27/01/2024 - entre 18:00 e 22:00</p>
+        </div>
+        <div class="container2">
+            <div class="img-container">
+                <img class="gps-img" src="img/gps.png" alt="GPS logo">
+            </div>
+            <div class="text-container">
+                <p>Salão Celina Festas:</p>
+                <p>Rua Rio Comprido, 989 - Riacho das Pedras, Contagem - MG</p>
+            </div>
+        </div>
+        <div class="google-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.4449387943505!2d-44.048881699999995!3d-19.947782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa695861bc7e0cb%3A0xd2ab7ab7ba7f2a97!2sR.%20Rio%20Comprido%2C%20989%20-%20Riacho%20das%20Pedras%2C%20Contagem%20-%20MG%2C%2032280-070!5e0!3m2!1spt-BR!2sbr!4v1703733171312!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+
     <div style="text-align: center;">
-    &ensp; <a style="margin-top: 180px;" class="botoes" href="confirmar_presenca.php">Quero Confirmar Presença</a>
-    &ensp; <a class="botoes" href="compartilhar_fotos.php">Quero Compartilhar Fotos</a>
+        <a class="botoes" href="confirmar_presenca.php">Eu Vou! :-)</a>
+        <a class="botoes" href="compartilhar_fotos.php">Não Vou...</a>
     </div>
 </body>
 </html>
