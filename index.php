b42179b0d6de65dfe5f4d1df149eaa9d79e6278a
Marlon Bertuani <31507265+marlonbertuani@users.noreply.github.com>
2023-12-28 03:50:00 +0000

    Atualizando estruturas e codigo, ajustando para mobile.

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 </head>
 <body>
     <div class="image-container">
-        <img class="logoarthur" src="superarthur.png" alt="Super Arthur Logo">
+        <img class="logoarthur" src="img/superarthur.png" alt="Super Arthur Logo">
     </div>    
     <div class="content-container">
         <h1 class="msghome">Aniversário do Arthur!</h1>
         <h1 style="font-size: large; text-align: center;">Confirme sua presença e nos ajude a organizar este momento especial!</h1>
-        <p style="font-size: large; text-align: center;">Prazo para confirmações até 17/01/2024 - 23:55</p>
+        <p style="font-size: medium; text-align: center;">Prazo para confirmações até 17/01/2024 - 23:55</p>
         
         <div class="container">
             <img class="calendar-img" src="img/calendar.png" alt="calendar logo">
@@ -29,7 +29,7 @@
             </div>
         </div>
         <div class="google-map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.4449387943505!2d-44.048881699999995!3d-19.947782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa695861bc7e0cb%3A0xd2ab7ab7ba7f2a97!2sR.%20Rio%20Comprido%2C%20989%20-%20Riacho%20das%20Pedras%2C%20Contagem%20-%20MG%2C%2032280-070!5e0!3m2!1spt-BR!2sbr!4v1703733171312!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.4449387943505!2d-44.048881699999995!3d-19.947782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa695861bc7e0cb%3A0xd2ab7ab7ba7f2a97!2sR.%20Rio%20Comprido%2C%20989%20-%20Riacho%20das%20Pedras%2C%20Contagem%20-%20MG%2C%2032280-070!5e0!3m2!1spt-BR!2sbr!4v1703733171312!5m2!1spt-BR!2sbr" width="385" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 
